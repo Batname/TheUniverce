b170c3699fdc8141ad36b7330a4fa89f89a89d46
Denis Dubinin <dadubinin@gmail.com>
2017-01-24 22:12:27 +0000

    init

---
+++ Source/TheUniverse/TheUniverseGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TheUniverseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEUNIVERSE_API ATheUniverseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
